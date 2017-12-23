dabad2bafb93928d25865151753e5c16fd3094ce
James Hughes <hugjimbo77@gmail.com>
2017-12-23 17:42:36 +0000

    file structure setup

---
+++ test/chucknorrisJokesSpec.js
@@ -0,0 +1,5 @@
+var expect = require('chai').expect;
+var TodoList = require('../index.js');
+const request = require('request');
+const supertest = require('supertest');
+var app = require("../server.js").app;
